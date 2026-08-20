cdbf4329bd0eb4d4043d6d99b3483b3a17145108
eunjin90 <eunjin900808@gmail.com>
2026-08-20 20:28:13 +0900

    Add files via upload

--- day01/DoWhile01.java
+++ day01/DoWhile01.java
@@ -10,12 +10,17 @@ public class DoWhile01 {
 		 * [DoWhile01] 숫자를 계속 입력 받으면서 더하다가 0을 입력하면 멈추고 지금까지 더한 값을 출력
 		 */
 		
-		System.out.println("숫자를 입력하세요 : ");
+		int sum = 0;
 		
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
+		int num;
+		do {
+			System.out.print("숫자를 입력하세요: ");
+			num = sc.nextInt();
+			sum += num;
+		}while(num != 0);
 		
-		
+		System.out.println("지금까지 입력한 숫자의 합은 "+ sum + "입니다.");
 
 	}
 
