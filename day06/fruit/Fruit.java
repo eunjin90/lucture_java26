34df91da8d6ddd0712959ab4613f8ce4e4e99286
eunjin90 <eunjin900808@gmail.com>
2026-09-02 10:31:32 +0900

    Add files via upload

---
+++ day06/fruit/Fruit.java
@@ -0,0 +1,30 @@
+package fruit;
+
+public class Fruit {
+
+	String name;
+	int price;
+	
+	public Fruit(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String toString() {
+		return (name + "(" + price + ")");
+	}
+
+	@Override
+	public int hashCode() {
+		return name != null ? name.hashCode() : 0;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Fruit)
+			return ((Fruit)obj).name.equals(name);
+		return false;
+	}
+	
+	
+}
